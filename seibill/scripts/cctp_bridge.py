effef948e834ea7d41824fc2f61d75354bf761cb
Pray4Lovee <totalwine2338@gmail.com>
2025-09-13 01:01:08 -0500

    Update cctp_bridge.py

--- seibill/scripts/cctp_bridge.py
+++ seibill/scripts/cctp_bridge.py
@@ -4,8 +4,6 @@ All rights reserved. This software and its source code may not be copied, modifi
 distributed, or used without prior written permission from the authors.
 
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
