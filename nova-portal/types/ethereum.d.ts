52961a6e7cbf533797717daefb53fdb297770cf8
Pray4Lovee <totalwine2338@gmail.com>
2025-09-28 14:00:54 -0500

    feat: add lumen card vault stack

---
+++ nova-portal/types/ethereum.d.ts
@@ -0,0 +1,11 @@
+interface EthereumProvider {
+  request: (args: { method: string; params?: unknown[] }) => Promise<any>;
+}
+
+declare global {
+  interface Window {
+    ethereum?: EthereumProvider;
+  }
+}
+
+export {};
